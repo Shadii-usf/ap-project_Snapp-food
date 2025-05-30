c67b4409da1ea86eaaa2b1198009d68ea6c5324d
Shadi Yousefabadi <shadiyoosefabadi@gmail.com>
2025-05-30 22:50:19 +0330

    add files

---
+++ src/main/java/org.example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        port(8080);
+
+        get("/hello", (req, res) -> "helloooooooo");
+
+        post("/auth/register", (req, res) -> "register user");
+
+        post("/echo", (req, res) -> {
+            String body = req.body();
+            if (body == null || body.isEmpty()) {
+                return "no input received";
+            }
+            return "you said " + body;
+        });
+
+        get("/", (req, res) -> {
+
+            return "{\"message\":\"سرور بالا است\"}";
+        });
+    }
+}
